17e5400aeeb873d5a8048b789ddc4e386f57ace5
ndphucc <ntamque@gmail.com>
2022-05-17 09:09:44 +0700

    a

--- module2/src/ss8_clean_code_refactoring/bai_tap/tennis_game/TennisGame.java
+++ module2/src/ss8_clean_code_refactoring/bai_tap/tennis_game/TennisGame.java
@@ -1,62 +1,65 @@
 package ss8_clean_code_refactoring.bai_tap.tennis_game;
 
 public class TennisGame {
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+    public static final String LOVE_ALL = "Love-All";
+    public static final String FIFTEEN_ALL = "Fifteen-All";
+    public static final String THIRTY_ALL = "Thirty-All";
+    public static final String FORTY_ALL = "Forty-All";
+    public static final String DEUCE = "Deuce";
+    public static final int HIT = 4;
 
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
+    public static String getScore(String player1Name, String player2Name, int scorePlayer1, int scorePlayer2) {
+        String score = "";
+        int tempScore = 0;
+        boolean deuce = (scorePlayer1 == scorePlayer2);
+        if (deuce) {
+            score += getDeuce(scorePlayer1);
+        } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
+            int minusResult = scorePlayer1 - scorePlayer2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            for (int i = 1; i < HIT - 1; i++) {
+                if (i == 1) tempScore = scorePlayer1;
+                else {
+                    score += "-";
+                    tempScore = scorePlayer2;
                 }
+                score += getScore(tempScore);
             }
         }
         return score;
+    }
+
+    public static String getScore(int scoint) {
+        switch (scoint) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "";
+        }
+    }
+
+    public static String getDeuce(int score) {
+        switch (score) {
+            case 0:
+                return LOVE_ALL;
+            case 1:
+                return FIFTEEN_ALL;
+            case 2:
+                return THIRTY_ALL;
+            case 3:
+                return FORTY_ALL;
+            default:
+                return DEUCE;
+        }
     }
 }
