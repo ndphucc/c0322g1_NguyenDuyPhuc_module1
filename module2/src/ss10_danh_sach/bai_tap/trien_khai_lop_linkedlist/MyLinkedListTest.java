47bf9e01e0b3a02940170e99546dbcba4f57934a
ndphucc <ntamque@gmail.com>
2022-05-23 08:29:42 +0700

    a

--- module2/src/ss10_danh_sach/bai_tap/trien_khai_lop_linkedlist/MyLinkedListTest.java
+++ module2/src/ss10_danh_sach/bai_tap/trien_khai_lop_linkedlist/MyLinkedListTest.java
@@ -3,11 +3,11 @@ package ss10_danh_sach.bai_tap.trien_khai_lop_linkedlist;
 public class MyLinkedListTest {
     public static void main(String[] args) {
         MyLinkedList<String> list = new MyLinkedList<>();
-        list.add(0,"Phuc");
-        list.add(1,"hai");
-        list.add(2,"ba");
-        list.add(3,"bon");
-        list.add(4,"nam");
+        list.add(0, "Phuc");
+        list.add(1, "hai");
+        list.add(2, "ba");
+        list.add(3, "bon");
+        list.add(4, "nam");
         list.addLast("Bao");
         list.remove(1);
         list.remove("bon");
